8c40fdc1fb31f37e4585bad52172b82ce3972e6b
nddoshi <nddoshi9@gmail.com>
2022-01-18 15:56:18 -0500

    updated how labels are computed and tested rotations

--- source/polyhedron_dataset.py
+++ source/polyhedron_dataset.py
@@ -3,10 +3,12 @@ import ipdb
 import numpy as np
 import os
 import pickle
+import plotly.subplots
 import plotly.graph_objects as pgo
 from torch.utils.data import Dataset
 
-import source.polyhedron_utils
+# import source.polyhedron_utils
+import polyhedron_utils
 
 
 class PolyhedronDataSet(Dataset):
@@ -17,7 +19,7 @@ class PolyhedronDataSet(Dataset):
         self.transform = transform
 
         self.data = []
-        self.labels = []
+        self.nfaces = []
 
         for dir in sorted(os.listdir(self.data_dir)):
             dir_path = os.path.join(data_dir, dir)
@@ -37,7 +39,7 @@ class PolyhedronDataSet(Dataset):
                         polyhedron_dir_path = os.path.join(self.data_dir,
                                                            '_'.join(file.split('_')[:2]))
 
-                        self.labels.append(polyhedron_info['n_faces'])
+                        self.nfaces.append(polyhedron_info['n_faces'])
 
                         if pc_type == 'drake_point_cloud':
                             drake_pc_path = os.path.join(polyhedron_dir_path,
@@ -51,10 +53,12 @@ class PolyhedronDataSet(Dataset):
                             raise RuntimeError("incorrect point cloud type")
 
         ct, self.class_dict = 0, {}
-        for label in self.labels:
-            if not label in self.class_dict:
-                self.class_dict[label] = ct
+        for nface in sorted(self.nfaces):
+            if not nface in self.class_dict:
+                self.class_dict[nface] = ct
                 ct += 1
+
+        self.labels = [self.class_dict[nface] for nface in self.nfaces]
 
     def __len__(self):
         return len(self.labels)
@@ -68,42 +72,52 @@ class PolyhedronDataSet(Dataset):
             pointcloud = self.transform(pointcloud)
 
         return {'pointcloud': pointcloud,
-                'class': self.class_dict[label]}
+                'class': label}
 
     def plot_sample(self, idx):
         ''' plot sample'''
+        # print(self.data[idx])
 
         pointcloud = np.load(self.data[idx])
-        label = self.labels[idx]
 
         if self.transform:
             pointcloud = self.transform(pointcloud)
 
         # plot sampled points
-        data = [pgo.Scatter3d(x=pointcloud[:, 0],
-                              y=pointcloud[:, 1],
-                              z=pointcloud[:, 2],
-                              name=label,
-                              mode='markers',
-                              marker_size=4,
-                              marker_symbol='circle',
-                              marker_color='rgba(0, 0, 255, 1)')]
+        data = pgo.Scatter3d(x=pointcloud[:, 0],
+                             y=pointcloud[:, 1],
+                             z=pointcloud[:, 2],
+                             mode='markers',
+                             marker_size=1,
+                             marker_symbol='circle',
+                             marker_color='rgba(0, 0, 255, 1)')
 
         return data
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     pc_type = 'ideal_point_cloud'
-#     data_dir = '/home/nddoshi/Research/learning_sandbox/datasets/Polygon'
+    pc_type = 'ideal_point_cloud'
+    data_dir = '/home/nddoshi/Research/learning_sandbox/datasets/Polygon'
 
-#     dataset = PolyhedronDataSet(
-#         pc_type=pc_type, data_dir=data_dir,
-#         transform=polyhedron_utils.train_transforms(scale=0.02))
+    dataset = PolyhedronDataSet(
+        pc_type=pc_type, data_dir=data_dir,
+        transform=polyhedron_utils.train_transforms(scale=0.02))
 
-#     sample_ind = np.random.randint(0, dataset.__len__()-1)
-#     sample = dataset.__getitem__(sample_ind)
-#     plot_data = dataset.plot_sample(sample_ind)
+    sample_ind = np.random.randint(0, dataset.__len__()-1)
+    sample = dataset.__getitem__(sample_ind)
+    print(sample['class'])
+    print(os.path.splitext(dataset.data[sample_ind])[0] + '.html')
+    label = dataset.labels[sample_ind]
+    nface = dataset.nfaces[sample_ind]
+    ncols = 2
+    fig = plotly.subplots.make_subplots(rows=2, cols=ncols,
+                                        specs=[[{"type": "scene"}] * ncols]*2,
+                                        subplot_titles=[f"Class: {label}, Sides: {nface}"]*2*ncols)
 
-#     figure = pgo.Figure(plot_data)
-#     figure.show()
+    for i in range(2 * ncols):
+        plot_data = dataset.plot_sample(sample_ind)
+        fig.add_trace(plot_data, row=i//ncols + 1, col=i % ncols + 1)
+
+    print(dataset.class_dict)
+    fig.show()

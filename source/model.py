70f6360b8ace07795e14000ad7c42e378d2e82c4
nddoshi <nddoshi9@gmail.com>
2022-02-02 23:12:25 -0500

    updated NN structure to be more flexible

--- source/model.py
+++ source/model.py
@@ -67,18 +67,10 @@ class Transform(nn.Module):
 
         self.input_transform = Tnet(k=3)
         self.feature_transform = Tnet(k=64)
-        self.critical_pt_dims = None
 
-        self.conv1 = nn.Conv1d(
-            in_channels=3, out_channels=64, kernel_size=1)
-        self.conv2 = nn.Conv1d(
-            in_channels=64, out_channels=128, kernel_size=1)
-        self.conv3 = nn.Conv1d(
-            in_channels=128, out_channels=1024, kernel_size=1)
-
-        self.bn1 = nn.BatchNorm1d(num_features=64)
-        self.bn2 = nn.BatchNorm1d(num_features=128)
-        self.bn3 = nn.BatchNorm1d(num_features=1024)
+        self.conv1 = conv(input_dim=3, output_dim=64)
+        self.conv2 = conv(input_dim=64, output_dim=128)
+        self.conv3 = conv(input_dim=128, output_dim=1024)
 
     def forward(self, input):
         # input.shape == (bs, n, 3)
@@ -88,14 +80,18 @@ class Transform(nn.Module):
         matrix3x3 = self.input_transform(input)
         xb1 = torch.bmm(torch.transpose(input, 1, 2),
                         matrix3x3).transpose(1, 2)
-        xb2 = nn.functional.relu(self.bn1(self.conv1(xb1)))
 
+        # convolutional layers
+        xb2 = self.conv1(xb1)
+
+        # batch matrix multiplication
         matrix64x64 = self.feature_transform(xb2)
         xb3 = torch.bmm(torch.transpose(xb2, 1, 2),
                         matrix64x64).transpose(1, 2)
 
-        xb4 = nn.functional.relu(self.bn2(self.conv2(xb3)))
-        xb5 = nn.functional.relu(self.bn3(self.conv3(xb4)))
+        # more convolutional layers
+        xb4 = self.conv2(xb3)
+        xb5 = self.conv3(xb4)
 
         xb6, crit_pt_inds = nn.MaxPool1d(
             xb5.size(-1), return_indices=True)(xb5)
@@ -112,18 +108,17 @@ class PointNet(nn.Module):
 
         self.transform = Transform()
 
-        self.fc1 = nn.Linear(in_features=1024, out_features=512)
+        self.fc1 = fully_connected(input_dim=1024, output_dim=512)
         self.fc2 = nn.Linear(in_features=512, out_features=256)
         self.fc3 = nn.Linear(in_features=256, out_features=classes)
 
-        self.bn1 = nn.BatchNorm1d(num_features=512)
-        self.bn2 = nn.BatchNorm1d(num_features=256)
+        self.bn = nn.BatchNorm1d(num_features=256)
         self.dropout = nn.Dropout(p=0.3)
         self.logsoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input):
         xb1, crit_pt_inds, matrix3x3, matrix64x64 = self.transform(input)
-        xb2 = nn.functional.relu(self.bn1(self.fc1(xb1)))
-        xb3 = nn.functional.relu(self.bn2(self.dropout(self.fc2(xb2))))
+        xb2 = self.fc1(xb1)
+        xb3 = nn.functional.relu(self.bn(self.dropout(self.fc2(xb2))))
         output = self.fc3(xb3)
         return self.logsoftmax(output), crit_pt_inds, matrix3x3, matrix64x64
